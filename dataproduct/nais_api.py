ef856876869bb9998646042f59ea5d2f1336a604
Kyrre Havik <kyrre.havik@nav.no>
2025-12-17 12:28:51 +0100

    feat: håndter feil, og hent 500 om gangen

--- dataproduct/nais_api.py
+++ dataproduct/nais_api.py
@@ -2,8 +2,9 @@ from .models import Deployment
 import requests
 
 DEPLOYMENTS_QUERY = """query GetDeployments($filters: DeploymentFilter, $after: Cursor) {
-  deployments (filter: $filters, after: $after) {
+  deployments (filter: $filters, after: $after, first: 500) {
     pageInfo {
+      totalCount
       hasNextPage
       endCursor
     }
@@ -30,18 +31,30 @@ def get_deployments(apikey: str, since: str):
         },
         'after': None
     }
-    response = requests.post("https://console.nav.cloud.nais.io/graphql",
-                             headers={"Authorization": "Bearer " + apikey},
-                             json={'query': DEPLOYMENTS_QUERY, 'variables': variables})
-    payload = response.json()
 
     deployments = []
     while True:
+        try:
+            response = requests.post("https://console.nav.cloud.nais.io/graphql",
+                                     timeout=10,
+                                     headers={
+                                         "Authorization": "Bearer " + apikey},
+                                     json={'query': DEPLOYMENTS_QUERY, 'variables': variables})
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            print("An error occurred:", err)
+            exit(1)
+
+        payload = response.json()
+
         for deployment in payload['data']['deployments']['nodes']:
             dep = Deployment(**deployment)
             deployments.extend(dep.to_dict())
 
         pageInfo = payload['data']['deployments']['pageInfo']
+        print(
+            f"Fetched {len(payload['data']['deployments']['nodes'])} deployments, total so far: {len(deployments)}, of total available: {pageInfo['totalCount']}")
+
         if pageInfo['hasNextPage']:
             variables['after'] = pageInfo['endCursor']
         else:
